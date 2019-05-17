216fa217cdf0185aab96efcfa4eb9d66ac25983c
Kunal Dahiya <cs11b15m000001@iith.ac.in>
2019-05-17 18:52:53 +0530

    Added hdf5; Introduced _file attr

--- deepxml/libs/lookup.py
+++ deepxml/libs/lookup.py
@@ -1,5 +1,7 @@
 import numpy as np
 import _pickle as pickle
+import h5py
+
 
 class Table(object):
     """
@@ -12,6 +14,7 @@ class Table(object):
         self._shape = None
         self.data = None
         self.data_init = False
+        self._file = None
 
     def _get_fname(self, fname, mode='data'):
         if mode == 'data':
@@ -19,7 +22,7 @@ class Table(object):
         else:
             return fname + ".metadata"
 
-    def create(self, _data, _fname):
+    def create(self, _data, _fname, *args, **kwargs):
         """
             Create a file
             Will copy data
@@ -29,12 +32,17 @@ class Table(object):
         if self._type == 'memory':
             self.data = _data
         elif self._type == 'memmap':
-            data = np.memmap(self._get_fname(_fname), dtype=self._dtype, mode='w+', shape=self._shape)
-            data[:] = _data[:]
-            del data, _data  # Save to disk and delete object in write mode
-            self.data = np.memmap(self._get_fname(_fname), dtype=self._dtype, shape=self._shape, mode='r')  # Open in read mode
+            self.data = np.memmap(self._get_fname(_fname), dtype=self._dtype, mode='w+', shape=self._shape)
+            self._file = self.data
+            self.data[:] = _data[:]
+            self.data.flush()
+            del _data  # Save to disk and delete object in write mode
         elif self._type == 'hdf5':
-            pass
+            self._file = h5py.File(self._get_fname(_fname), 'w+')
+            self._file.create_dataset('data', data=_data)
+            self.data.flush()
+            self.data = self._file.get('data')
+            del _data
         elif self._type == 'pytables':
             pass
         else:
@@ -49,6 +57,16 @@ class Table(object):
         pickle.dump(obj, open(self._get_fname(_fname, 'metadata'), 'wb'))
         if self._type == 'memory': # Save numpy array; others are already on disk
             np.save(self._get_fname(_fname), self.data)
+        elif self._type == 'hdf5': #Not expected to work when filenames are same
+            _file = h5py.File(self._get_fname(_fname), 'w+')
+            _file.create_dataset('data', data=self.data)
+            _file.close()            
+        elif self._type == 'memmap': #Not expected to work when filenames are same
+            _file = np.memmap(self._get_fname(_fname), dtype=self._dtype, mode='w+', shape=self._shape)
+            _file[:] = self.data[:]
+            _file.flush()
+        else:
+            raise NotImplementedError("Unknown type!")
 
     def load(self, _fname):
         obj = pickle.load(open(self._get_fname(_fname, 'metadata'), 'rb'))
@@ -58,9 +76,10 @@ class Table(object):
         if self._type == 'memory':
             self.data = np.load(self._get_fname(_fname), allow_pickle=True)
         elif self._type == 'memmap':
-            self.data = np.memmap(self._get_fname(_fname), mode='r', shape=self._shape, dtype=self._dtype)
+            self.data = np.memmap(self._get_fname(_fname), mode='r+', shape=self._shape, dtype=self._dtype)
         elif self._type == 'hdf5':
-            pass
+            fp = h5py.File(self._get_fname(_fname), 'r+')
+            self.data = fp.get('data')
         elif self._type == 'pytables':
             pass
         else:
@@ -83,14 +102,22 @@ class PartitionedTable(object):
         for _ in range(self.num_partitions):
             self.data.append(Table(_type, _dtype))
 
-    def create(self, _data, _fname):
+    def _create_one(self, _data, _fname, idx): # Create a specific graph only
+        #TODO: Add condition to check for invalid idx
+        #TODO: Good way to set data_init condition if individual graphs are set
+        self.data[idx].create(_data, _fname + ".{}".format(idx))
+
+    def create(self, _data, _fname, idx=-1):
         """
             Create a file
             Will copy data
         """
-        for idx in range(self.num_partitions):
-            self.data[idx].create(_data[idx], _fname + ".{}".format(idx))
-        self.data_init = True
+        if idx != -1:
+            self._create_one(_data, _fname, idx)
+        else:
+            for idx in range(self.num_partitions):
+                self._create_one(_data[idx], _fname, idx)
+            self.data_init = True
 
     def query(self, indices):
         """
@@ -112,3 +139,6 @@ class PartitionedTable(object):
         for idx in range(self.num_partitions):
             self.data[idx].load(_fname + ".{}".format(idx))
         self.data_init = True
+
+    def set_status(self, _status):
+        self.data_init = _status
