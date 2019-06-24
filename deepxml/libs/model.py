0dfe7f65919440a0aa7768ad596203c98d74bc9c
Kunal Dahiya <cs11b15m000001@iith.ac.in>
2019-06-24 21:34:26 +0530

    Integrated new data format in pp_with_shortlist

--- deepxml/libs/model.py
+++ deepxml/libs/model.py
@@ -22,19 +22,30 @@ class ModelFull(ModelBase):
         super().__init__(params, net, criterion, optimizer)
         self.feature_indices = params.feature_indices
 
-    def _pp_with_shortlist(self, shorty, data_dir, dataset, fname='train.txt', 
-                           data=None, keep_invalid=False, batch_size=128, 
-                           num_workers=4, data_loader=None, **kwargs):
+    def _pp_with_shortlist(self, shorty, data_dir, dataset, tr_feat_fname='trn_X_Xf.txt',
+                           tr_label_fname='trn_X_Y.txt', normalize_features=True,
+                           normalize_labels=False, data=None, keep_invalid=False,
+                           feature_indices=None, label_indices=None, batch_size=128,
+                           num_workers=4, data_loader=None):
+        """
+            Post-process with shortlist. Train an ANN without touching the classifier 
+        """
         if data_loader is None:
             dataset = self._create_dataset(os.path.join(data_dir, dataset),
-                                        fname=fname,
-                                        data=data,
-                                        mode='predict',
-                                        keep_invalid=keep_invalid,
-                                        **kwargs)
+                                           fname_features=tr_feat_fname,
+                                           fname_labels=tr_label_fname,
+                                           data=data,
+                                           mode='predict',
+                                           keep_invalid=keep_invalid,
+                                           normalize_features=normalize_features,
+                                           normalize_labels=normalize_labels,
+                                           feature_indices=feature_indices,
+                                           )
+
             data_loader = self._create_data_loader(dataset,
-                                                batch_size=batch_size,
-                                                num_workers=num_workers)
+                                                   batch_size=batch_size,
+                                                   num_workers=num_workers,
+                                                   shuffle=False)
 
         self.logger.info("Post-processing with shortlist!")
         shorty.reset()
@@ -61,10 +72,11 @@ class ModelShortlist(ModelBase):
         return beta*torch.sigmoid(out_logits) + (1-beta)*torch.sigmoid(1-batch_dist)
 
     def _combine_scores(self, out_logits, batch_dist, beta):
-        if isinstance(out_logits, list): #For distributed classifier
+        if isinstance(out_logits, list):  # For distributed classifier
             out = []
             for _, (_out_logits, _batch_dist) in enumerate(zip(out_logits, batch_dist)):
-                out.append(self._combine_scores_one(_out_logits.data.cpu(), _batch_dist.data, beta))
+                out.append(self._combine_scores_one(
+                    _out_logits.data.cpu(), _batch_dist.data, beta))
             return out
         else:
             return self._combine_scores_one(out_logits.data.cpu(), batch_dist.data, beta)
@@ -76,11 +88,13 @@ class ModelShortlist(ModelBase):
             del val
         return stripped_vals
 
-    def _update_predicted_shortlist(self, count, batch_size, predicted_labels, batch_out, batch_data, beta, top_k=50):
+    def _update_predicted_shortlist(self, count, batch_size, predicted_labels, batch_out, 
+                                    batch_data, beta, top_k=50):
         _score = self._combine_scores(batch_out, batch_data['Y_d'], beta)
-        if 'Y_m' in batch_data: # IF rev mapping exist; case of distributed classifier
+        if 'Y_m' in batch_data:  # IF rev mapping exist; case of distributed classifier
             batch_shortlist = batch_data['Y_m'].numpy()
-            _knn_score = 1 - torch.cat(batch_data['Y_d'], 1) # Send this as merged?
+            # Send this as merged?
+            _knn_score = 1 - torch.cat(batch_data['Y_d'], 1)
             _clf_score = torch.cat(batch_out, 1).data
             _score = torch.cat(_score, 1)
         else:
@@ -94,7 +108,6 @@ class ModelShortlist(ModelBase):
             batch_shortlist, top_k)
         utils.update_predicted_shortlist(
             count, batch_size, _score, predicted_labels['combined'], batch_shortlist, top_k)
-
 
     def _validate(self, data_loader, beta=0.2):
         self.net.eval()
@@ -117,7 +130,8 @@ class ModelShortlist(ModelBase):
             out_ans = self.net.forward(batch_data)
             loss = self._compute_loss(out_ans, batch_data)/batch_size
             mean_loss += loss.item()*batch_size
-            self._update_predicted_shortlist(count, batch_size, predicted_labels, out_ans, batch_data, beta)
+            self._update_predicted_shortlist(
+                count, batch_size, predicted_labels, out_ans, batch_data, beta)
             count += batch_size
             if batch_idx % self.progress_step == 0:
                 self.logger.info(
@@ -125,7 +139,8 @@ class ModelShortlist(ModelBase):
         return self._strip_padding_label(predicted_labels, num_labels), mean_loss / \
             data_loader.dataset.num_samples
 
-    def _fit(self, train_loader, train_loader_shuffle, validation_loader, model_dir, result_dir, init_epoch, num_epochs, beta):
+    def _fit(self, train_loader, train_loader_shuffle, validation_loader, model_dir, 
+             result_dir, init_epoch, num_epochs, beta):
         for epoch in range(init_epoch, init_epoch+num_epochs):
             if epoch != 0 and self.dlr_step != -1 and epoch % self.dlr_step == 0:
                 self._adjust_parameters()
@@ -136,10 +151,12 @@ class ModelShortlist(ModelBase):
                 shorty_start_t = time.time()
                 self.shorty.reset()
                 shortlist_utils.update(
-                    train_loader, self, self.embedding_dims, self.shorty, flag=0, num_graphs=self.num_clf_partitions)
+                    train_loader, self, self.embedding_dims, self.shorty, 
+                    flag=0, num_graphs=self.num_clf_partitions)
                 if validation_loader is not None:
                     shortlist_utils.update(
-                        validation_loader, self, self.embedding_dims, self.shorty, flag=2, num_graphs=self.num_clf_partitions)
+                        validation_loader, self, self.embedding_dims, self.shorty, 
+                        flag=2, num_graphs=self.num_clf_partitions)
                 shorty_end_t = time.time()
                 self.logger.info("ANN train time: {} sec".format(
                     shorty_end_t - shorty_start_t))
@@ -183,7 +200,6 @@ class ModelShortlist(ModelBase):
         self.logger.info("Training time: {} sec, Validation time: {} sec, Shortlist time: {} sec".format(
             self.tracking.train_time, self.tracking.validation_time, self.tracking.shortlist_time))
 
-
     def fit(self, data_dir, model_dir, result_dir, dataset, learning_rate, num_epochs, data=None,
             tr_feat_fname='trn_X_Xf.txt', tr_label_fname='trn_X_Y.txt', val_feat_fname='tst_X_Xf.txt',
             val_label_fname='tst_X_Y.txt', batch_size=128, num_workers=4, shuffle=False,
@@ -206,9 +222,9 @@ class ModelShortlist(ModelBase):
                                                 num_workers=num_workers,
                                                 shuffle=False)
         train_loader_shuffle = self._create_data_loader(train_dataset,
-                                                batch_size=batch_size,
-                                                num_workers=num_workers,
-                                                shuffle=shuffle)
+                                                        batch_size=batch_size,
+                                                        num_workers=num_workers,
+                                                        shuffle=shuffle)
         self.logger.info("Loading validation data.")
         print("Loading validation data.", val_feat_fname, val_label_fname)
         validation_loader = None
@@ -229,7 +245,6 @@ class ModelShortlist(ModelBase):
         self._fit(train_loader, train_loader_shuffle, validation_loader,
                   model_dir, result_dir, init_epoch, num_epochs, beta)
 
-
     def _predict(self, data_loader, **kwargs):
         beta = kwargs['beta'] if 'beta' in kwargs else 0.5
         self.logger.info("Loading test data.")
@@ -241,7 +256,8 @@ class ModelShortlist(ModelBase):
         # TODO Add flag for loading or training
         if self.update_shortlist:
             shortlist_utils.update(
-                data_loader, self, self.embedding_dims, self.shorty, flag=2, num_graphs=self.num_clf_partitions)
+                data_loader, self, self.embedding_dims, self.shorty, 
+                flag=2, num_graphs=self.num_clf_partitions)
         else:
             try:
                 _fname = kwargs['shorty_fname']
@@ -276,8 +292,10 @@ class ModelShortlist(ModelBase):
 
     def save_checkpoint(self, model_dir, epoch):
         super().save_checkpoint(model_dir, epoch)
-        self.tracking.saved_checkpoints[-1]['ANN'] = 'checkpoint_ANN_{}.pkl'.format(epoch) 
-        self.shorty.save(os.path.join(model_dir, self.tracking.saved_checkpoints[-1]['ANN']))
+        self.tracking.saved_checkpoints[-1]['ANN'] = 'checkpoint_ANN_{}.pkl'.format(
+            epoch)
+        self.shorty.save(os.path.join(
+            model_dir, self.tracking.saved_checkpoints[-1]['ANN']))
         self.purge(model_dir)
 
     def load_checkpoint(self, model_dir, fname, epoch):
